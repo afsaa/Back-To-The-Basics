9a9c85e4848d9fbe2420f61ea20babbe717b27f5
Andrés Saam <andressaa.dev@gmail.com>
2019-09-04 11:28:35 -0500

    Mostrando los datos de 7 personajes usando el método map

--- JavasCript-Basics/Promesas.js
+++ JavasCript-Basics/Promesas.js
@@ -1,17 +1,19 @@
 const API_URL = `https://swapi.co/api/`;
-const PEOPLE_URL = `${API_URL}people/:id`.replace(":id", 1);
+const people = [1, 2, 3, 4, 5, 6, 7];
 
-$.get(PEOPLE_URL, function(data) {
-  let promesaPersona = new Promise((resolve, reject) => {
-    resolve(`Se cumplió la promesa`);
-    reject(`No se cumplió la promesa`);
-  });
+people.map(id => {
+  let PEOPLE_URL = `${API_URL}people/:id`.replace(":id", id);
+  $.get(PEOPLE_URL, function(data) {
+    let promesaPersona = new Promise((resolve, reject) => {
+      resolve(`Se cumplió la promesa`);
+      reject(`No se cumplió la promesa`);
+    });
 
-  promesaPersona.then(() => {
-    console.log(`Mi nombre es ${data.name}`);
-    $("body").html(`<div><h1>Mi nombre es ${data.name}</h1></div>`);
-  });
-  promesaPersona.catch(() => {
-    console.log(`No se pudo completar la acción`);
+    promesaPersona
+      .then(() => {
+        console.log(`Mi nombre es ${data.name}`);
+        $("body").append(`<div><h1>Mi nombre es ${data.name}</h1></div>`);
+      })
+      .catch(() => console.log(`No se pudo completar la acción`));
   });
 });
